841609225cbebb115fad527bd25ed688bf16a076
Avicenna <maverickbozo@me.com>
2016-01-22 02:06:06 +0700

    line number

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -140,3 +140,7 @@
 ;; emmet-mode hook to other mode
 (add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
 (add-hook 'css-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
+
+;; line number
+(global-linum-mode t)
+
