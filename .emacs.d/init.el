0d205828269ab6f5b555754f7fc70f607bc214a8
Avicenna <maverickbozo@me.com>
2016-01-22 01:51:58 +0700

    more goodies for init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -111,3 +111,32 @@
 (add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
 
 (setq cider-auto-select-error-buffer nil)
+
+;; highlight matching paren
+(show-paren-mode 1)
+
+
+;; UNIQUIFY
+(require 'uniquify)
+
+(setq uniquify-buffer-name-style 'post-forward
+      uniquify-separator ":")
+
+
+
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.blade\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode)) (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode)) (add-to-list 'auto-mode-alist '("\\.[gj]sp\\'" . web-mode)) (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode)) (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode)) (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode)) (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2))
+
+(add-hook 'web-mode-hook 'my-web-mode-hook)
+
+
+;; emmet-mode hook to other mode
+(add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
+(add-hook 'css-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
